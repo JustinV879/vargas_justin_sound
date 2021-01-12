4827ce0fdaa79e5ca538c413995787a4a8a9b114
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2021-01-12 10:54:13 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     print("Sound Level:" + input.sound_level())
-    if (input.sound_level()) >= 20:
+    if (input.sound_level()) >= 10:
+        light.set_all(color.rgb(255,0,255))
         music.pew_pew.play()
         music.magic_wand.loop()
         music.set_volume(80)
-        
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 while (true) {
     console.log("Sound Level:" + input.soundLevel())
-    if (input.soundLevel() >= 20) {
+    if (input.soundLevel() >= 10) {
+        light.setAll(color.rgb(255, 0, 255))
         music.pewPew.play()
         music.magicWand.loop()
         music.setVolume(80)
