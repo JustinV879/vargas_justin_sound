54f5341aa998c383fec0aedc4192da14716ecd3c
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2021-01-14 13:22:45 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ while True:
     print("Light Level:" + input.light_level())
     if (input.light_level()) >= 20:
         music.pew_pew.play()
-        light.clear()
--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ while (true) {
     console.log("Light Level:" + input.lightLevel())
     if (input.lightLevel() >= 20) {
         music.pewPew.play()
-        light.clear()
     }
     
 }
